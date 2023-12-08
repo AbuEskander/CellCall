c938dde1281af8dfedb108511039f1fcef30059b
Basil Ismail <basselawni1@gmail.com>
2023-12-08 18:25:30 +0300

    Login /Register Setup done

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 import express from "express";
-import dotenv from "dotenv";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
+import mongooseEncryption from "mongoose-encryption";
+import bcrypt from "bcrypt";
+import expressSession from "express-session";
 
 mongoose.connect("mongodb://127.0.0.1:27017/cellcall");
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+app.use(
+  expressSession({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
-app.use(bodyParser.urlencoded({ extended: true }));
 const postSchema = new mongoose.Schema({
   title: String,
   content: String,
@@ -16,29 +31,77 @@ const postSchema = new mongoose.Schema({
 });
 const userSchema = new mongoose.Schema({
   name: String,
-  email: { type: String, unique: true },
+  email: {
+    type: String,
+    unique: true,
+  },
   password: String,
-  posts: [postSchema],
+  posts: Array,
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(mongooseEncryption, {
+  secret: secret,
+  encryptedFields: ["password"],
 });
 
 const User = mongoose.model("User", userSchema);
 const Post = mongoose.model("Post", postSchema);
 
-let basil = new User({
-  name: "Basil",
-  email: "HEhe",
-  password: "1234",
-  Date: Date.now(),
-});
-
 app.get("/", (req, res) => {
+  res.render("welcome.ejs");
+});
+app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
 
-// let ahmed = await User.findOne({ name: "Basil" }).exec();
+app.get("/home", (req, res) => {
+  res.render("home.ejs");
+});
 
-// console.log(ahmed.posts);
-
+app.post("/register", async (req, res) => {
+  try {
+    const exist = await User.findOne({ email: req.body.email });
+    if (!exist) {
+      const hashed = await bcrypt.hash(req.body.password, saltRounds);
+      const newUSER = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashed,
+      });
+      newUSER.save();
+      res.redirect("/home");
+    } else {
+      res.redirect("/login");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/login", async (req, res) => {
+  try {
+    const exist = await User.findOne({ email: req.body.email });
+    if (exist) {
+      bcrypt.compare(req.body.password, exist.password, (err, match) => {
+        if (match) {
+          res.redirect("/home");
+          console.log("password is correct");
+        } else {
+          res.redirect("/login");
+          console.log("password is incorrect");
+        }
+      });
+    }else{
+      res.redirect("/register");
+      console.log("email is incorrect");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
 app.listen(port, (err) =>
   err ? console.log(err) : console.log(`Server is running on port ${port}`)
 );
